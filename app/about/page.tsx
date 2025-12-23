a23582aea694fea80ac5fe5f86ff0117884c207d
Peter Sinkovicz <antsticky@gmail.com>
2025-12-23 18:40:41 +0100

    rename

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 export default async function AboutPage() {
-  const apiUrl = process.env.FASTAPI_URL;
-  console.log("FASTAPI_URL =", process.env.FASTAPI_URL);
+  const apiUrl = process.env.NEXT_PUBLIC_FASTAPI_URL;
+  console.log("FASTAPI_URL =", process.env.NEXT_PUBLIC_FASTAPI_URL);
 
   const res = await fetch(apiUrl, { cache: "no-store" });
   const data = await res.json();
