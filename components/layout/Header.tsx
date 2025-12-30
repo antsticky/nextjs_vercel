10246444cc74f77ea97f831c2c89c844bb6380d9
Peter Sinkovicz <antsticky@gmail.com>
2025-12-30 20:20:38 +0100

    fix

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -38,6 +38,7 @@ export default function Header() {
   const handleNavigation = (tab: 'home' | 'about' | 'settings', path: string) => {
     setActiveTab(tab)
     setOpen(false)
+    setHoveredTab(null)
     router.push(path)
   }
 
