f38f4ada200c767f2f38ade505d85ee098ac1eda
Peter Sinkovicz <antsticky@gmail.com>
2026-01-02 00:33:54 +0100

    global loading

---
+++ app/(site)/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="routeLoaderOverlay">
+      <img src="/loader.gif" alt="Loading..." className="routeLoaderGif" />
+    </div>
+  )
+}
